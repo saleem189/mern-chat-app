d8fc64f85c6235c44dedf8dc4df913c9cba945cb
saleem ayoub <saleemayoub1@gmail.com>
2023-05-11 00:50:42 +0500

    minor change in api routes

--- Routes/api.js
+++ Routes/api.js
@@ -4,10 +4,14 @@ const LoginValidator = require("../Validations/LoginValidator");
 
 router.post("/login", (req, res) => {
 
+    // Validating the data from the form which is coming from the client
     const { errors, isValid } = LoginValidator(req.body);
+
     if (!isValid) {
         return res.status(400).json(errors);
     }else{
+        let email = req.body.email;
+        let password = req.body.password;
         return res.status(200).json({success:true});
     }
     // return res.status(404).json({ success: false, message: "Invalid Credentials" });
