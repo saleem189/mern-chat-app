6e027de25238e0aba3276cc007c5060a10518f9f
saleem ayoub <saleemayoub1@gmail.com>
2023-05-11 00:01:25 +0500

    :construction: working on validators

--- Routes/api.js
+++ Routes/api.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const LoginValidator = require("../Validations/LoginValidator");
 
 router.post("/login", (req, res) => {
-    // return res.status(200).json({success:true});
-    
+
+    const { errors, isValid } = LoginValidator(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }else{
+        return res.status(200).json({success:true});
+    }
+    // return res.status(404).json({ success: false, message: "Invalid Credentials" });
 });
 
 module.exports = router;
