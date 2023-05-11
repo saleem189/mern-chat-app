493e73ae8389abab08110dd5abdff31eb95912b1
saleem ayoub <saleemayoub1@gmail.com>
2023-05-11 22:13:24 +0500

    :refactor: re aranging and breaking code into function

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -6,27 +6,7 @@ const bcrypt = require("bcryptjs");
 
 const checkUser = ({email: email, password: password , name: name}, res) =>{
     User.findOne({email:email}).then(user => {
-        if(user){
-            return res.status(400).json({email: "Email already exists"});
-        }else{
-           const newUser = new User({
-            name: name,
-            email: email,
-            password: password
-           });
-
-           // Hash password before saving in database
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err;
-                newUser.password = hash;
-                newUser
-                    .save()
-                    .then(user => res.status(201).json({status:true, user:user}))
-                    .catch(err => res.status(500).json(err));
-                });
-            });
-        }
+        (user) ? res.status(400).json({email: "Email already exists"}) : createUser({name: name, email: email, password: password}, res);
     });
 }
 
@@ -40,6 +20,26 @@ const comparePassword = (password, hash, user, res) => {
     bcrypt.compare(password, hash).then(isMatch => {
         isMatch ? res.status(200).json({status:true, user:user}) : res.status(400).json({passwordincorrect: "Password incorrect"});
     });
+}
+
+const createUser = ({name: name, email: email, password: password}, res) =>{
+    const newUser = new User({
+        name: name,
+        email: email,
+        password: password
+       });
+
+       // Hash password before saving in database
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+                .save()
+                .then(user => res.status(201).json({status:true, user:user}))
+                .catch(err => res.status(500).json(err));
+            });
+        });
 }
 
 module.exports = {checkUser, loginUser};
